72fdb067a5ffe15d3a000ad1d6cf433eebb86414
JuneSunAt7 <jprogram@bk.ru>
2024-03-08 19:37:32 +0300

    create library architecture

--- headers/libs.h
+++ headers/libs.h
@@ -12,12 +12,19 @@ class Libs{
     private:
         void _create_libs_list();
         void _choose_operation(int &numOfOperation);
+        void _show_standart_sources();
+        void _change_sources();
+        void _add_lib();
+        void _add_source();
+        void _change_settings(std::list <std::string> &data);
+        std::list <std::string> _read_settings_file();
+        void _write_settings_file();
 
         std::list <std::string> standart_libs;
+        std::list <std::string> user_installed;
         std::list <std::string> sources;
 
     public:
         Libs();
-        void add_lib(std::string &lib);
-        void add_source(std::string &source);
+        
 };
--- src/libs.cpp
+++ src/libs.cpp
@@ -4,8 +4,10 @@
 Libs::Libs(){
     int numOfOperation;
     
-    std::cout << MAGENTA << "[1] " << YELLOW << "view all projects\n";
-    std::cout << MAGENTA << "[2] " << YELLOW << "delete project\n";
+    std::cout << MAGENTA << "[1] " << YELLOW << "standart libs sources\n";
+    std::cout << MAGENTA << "[2] " << YELLOW << "change sources\n";
+    std::cout << MAGENTA << "[3] " << YELLOW << "add lib\n";
+    std::cout << MAGENTA << "[4] " << YELLOW << "add sources\n";
 
     std::cout << RESET << "Number of operation: ";
     std::cin >> numOfOperation;
@@ -13,5 +15,34 @@ Libs::Libs(){
 }
 
 void Libs::_choose_operation(int &numOfOperation){
-    
+    switch (numOfOperation)
+    {
+    case 1:{
+         _show_standart_sources();
+        break;
+    }
+    case 2:{
+        _change_sources();
+        break;
+    }
+    case 3:{
+        _add_lib();
+        break;
+    }
+    case 4:{
+        _add_source();
+        break;
+    }
+    default:
+        break;
+    }
+}
+
+void Libs::_add_lib(){
+    std::string newLib;
+
+    std::cout << CYAN << "Name of lib: ";
+    std::cin >> newLib;
+
+    standart_libs.push_back(newLib);
 }
